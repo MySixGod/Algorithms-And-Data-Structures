839bdcb7f8b36ab411aaaa8b2dbb9aba4ab11e8f
unknown <gz0544@4399VDC.COM>
2021-05-21 10:03:39 +0800

    经典动态规划 不相交的线

--- leetcode/动态规划/不相交的线.py
+++ leetcode/动态规划/不相交的线.py
@@ -4,25 +4,25 @@ class Solution:
         # 检查输入参数
         size1 = len(nums1)
         size2 = len(nums2)
-    
+
         # 初始状态定义
         dp = [[0] * size2 for _ in range(size1)]
         dp[0][0] = 1 if nums1[0] == nums2[0] else 0
 
-        # 状态推导  
+        # 状态推导
         for i in range(size1):
             for j in range(size2):
-                if i == 0 and j == 0: continue
+                if i == 0 and j == 0:
+                    continue
                 if i == 0:
-                    dp[i][j] = 1 if nums2[j] == nums1[i] else dp[i][j-1]
+                    dp[i][j] = 1 if nums2[j] == nums1[i] else dp[i][j - 1]
                     continue
                 if j == 0:
-                    dp[i][j] = 1 if nums2[j] == nums1[i] else dp[i-1][j]
+                    dp[i][j] = 1 if nums2[j] == nums1[i] else dp[i - 1][j]
                     continue
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
                 if nums1[i] == nums2[j]:
-                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1)
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
 
-        # print(dp)    
-        return dp[size1-1][size2-1] 
-
+        # print(dp)
+        return dp[size1 - 1][size2 - 1]
